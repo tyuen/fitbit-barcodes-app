968622858211872b73db59d82c5d749f76c9ee16
tyuen <tyuen1@gmail.com>
2018-01-27 00:14:49 +0800

    Update index.jsx

--- settings/index.jsx
+++ settings/index.jsx
@@ -11,6 +11,11 @@ var allColors = [
   {color: "#808080"}  //grey
 ];
 
+var allTypes = [
+  {name: "Auto-Detect", value: "0"},
+  {name: "Code-128", value: "1"}
+];
+
 function getLen(str) {
   try {
     str = JSON.parse(str).name;
@@ -40,30 +45,35 @@ registerSettingsPage(props => {
         <TextInput settingsKey="name1" title="Name" placeholder="e.g., 7-Eleven" />
         <TextInput settingsKey="code1" label="Barcode" placeholder="e.g., 12345678" />
         <ColorSelect settingsKey="color1" colors={allColors} />
+        <Select settingsKey="type1" label="Encoding" options={allTypes} />
       </Section>
 
       <Section title="Barcode 2" description={getLen(props.settings.code2)}>
         <TextInput settingsKey="name2" title="Name"  placeholder="e.g., 7-Eleven" />
         <TextInput settingsKey="code2" title="Barcode" placeholder="e.g., 12345678" />
         <ColorSelect settingsKey="color2" colors={allColors} />
+        <Select settingsKey="type2" label="Encoding" options={allTypes} />
       </Section>
 
       <Section title="Barcode 3" description={getLen(props.settings.code3)}>
         <TextInput settingsKey="name3" title="Name"  placeholder="e.g., 7-Eleven" />
         <TextInput settingsKey="code3" title="Barcode" placeholder="e.g., 12345678" />
         <ColorSelect settingsKey="color3" colors={allColors} />
+        <Select settingsKey="type3" label="Encoding" options={allTypes} />
       </Section>
 
       <Section title="Barcode 4" description={getLen(props.settings.code4)}>
         <TextInput settingsKey="name4" title="Name"  placeholder="e.g., 7-Eleven" />
         <TextInput settingsKey="code4" title="Barcode" placeholder="e.g., 12345678" />
         <ColorSelect settingsKey="color4" colors={allColors} />
+        <Select settingsKey="type4" label="Encoding" options={allTypes} />
       </Section>
 
       <Section title="Barcode 5" description={getLen(props.settings.code5)}>
         <TextInput settingsKey="name5" title="Name"  placeholder="e.g., 7-Eleven" />
         <TextInput settingsKey="code5" title="Barcode" placeholder="e.g., 12345678" />
         <ColorSelect settingsKey="color5" colors={allColors} />
+        <Select settingsKey="type5" label="Encoding" options={allTypes} />
       </Section>
     </Page>
   );
