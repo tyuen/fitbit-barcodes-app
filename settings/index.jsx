0084ae2a8f14a414ed6feb139278fdd9f6ada7b3
Terry Yuen <tyuen1@gmail.com>
2018-05-22 11:05:39 +0800

    Update index.jsx

--- settings/index.jsx
+++ settings/index.jsx
@@ -12,31 +12,35 @@ var allColors = [
 ];
 
 var allTypes = [
-  {name: "Auto-Detect", value: "0"},
+  {name: "EAN/UPC/Code-128", value: "0"},
   {name: "Code-128", value: "1"},
   {name: "Code-39", value: "2"}
 ];
 
-function getLen(str) {
+function showWarning(str) {
+  try {
+    if(!JSON.parse(str).name) return null;
+  } catch(e) {
+    return null;
+  }
+  return (
+    <Section title={<Text>Tip: What does &quot;<Text bold>Code too long</Text>&quot; mean?</Text>}>
+      <Text>The more characters your barcode has, the more horizontal space is required to display the barcode. When the
+      required barcode space exceeds the physical screen space of your device, you will get a &quot;<Text italic>Code too long</Text>&quot; 
+      message, because the barcode cannot fit on the screen.</Text>
+    </Section>
+    )
+}
+
+function checkCode(str) {
   try {
     str = JSON.parse(str).name;
   } catch(e) {
     return null;
   }
-  let n = 4;
-  if(str.length < 4 || /[^0-9]/.test(str)) {
-    for(let i = 0; i < str.length; i++) {
-      let c = str.charCodeAt(i) - 32;
-      if(c < 0 || c > 94) return "Warning: Barcode has invalid letters";
-      n++;
-    }
-  } else {
-    let len = str.length;
-    if(len % 2 === 1) len--;
-    for(let i = 0; i < len; i += 2) n++;
-    if(len < str.length) n += 2;
-  }
-  return n > 15 ? "Warning: Barcode is "+n+" tuples, which exceeds 15 tuples." : null;
+  if(/^\s/.test(str)) return "Warning: barcode has empty spaces at the start.";
+  if(/\s$/.test(str)) return "Warning: barcode has empty spaces at the end.";
+  return null;
 }
 
 function prettyAgo(t) {
@@ -56,37 +60,38 @@ function prettyAgo(t) {
 registerSettingsPage(props => {
   return (
     <Page>
+      {showWarning(props.settings.code1)}
       <Toggle settingsKey="bright" label="Increase screen brightness" />
 
-      <Section title="Barcode 1" description={getLen(props.settings.code1)}>
+      <Section title="Barcode 1" description={checkCode(props.settings.code1)}>
         <TextInput settingsKey="name1" title="Name" placeholder="e.g., 7-Eleven" />
         <TextInput settingsKey="code1" label="Barcode" placeholder="e.g., 12345678" />
         <ColorSelect settingsKey="color1" colors={allColors} />
         <Select settingsKey="type1" label="Encoding" options={allTypes} />
       </Section>
 
-      <Section title="Barcode 2" description={getLen(props.settings.code2)}>
+      <Section title="Barcode 2" description={checkCode(props.settings.code2)}>
         <TextInput settingsKey="name2" title="Name"  placeholder="e.g., 7-Eleven" />
         <TextInput settingsKey="code2" title="Barcode" placeholder="e.g., 12345678" />
         <ColorSelect settingsKey="color2" colors={allColors} />
         <Select settingsKey="type2" label="Encoding" options={allTypes} />
       </Section>
 
-      <Section title="Barcode 3" description={getLen(props.settings.code3)}>
+      <Section title="Barcode 3" description={checkCode(props.settings.code3)}>
         <TextInput settingsKey="name3" title="Name"  placeholder="e.g., 7-Eleven" />
         <TextInput settingsKey="code3" title="Barcode" placeholder="e.g., 12345678" />
         <ColorSelect settingsKey="color3" colors={allColors} />
         <Select settingsKey="type3" label="Encoding" options={allTypes} />
       </Section>
 
-      <Section title="Barcode 4" description={getLen(props.settings.code4)}>
+      <Section title="Barcode 4" description={checkCode(props.settings.code4)}>
         <TextInput settingsKey="name4" title="Name"  placeholder="e.g., 7-Eleven" />
         <TextInput settingsKey="code4" title="Barcode" placeholder="e.g., 12345678" />
         <ColorSelect settingsKey="color4" colors={allColors} />
         <Select settingsKey="type4" label="Encoding" options={allTypes} />
       </Section>
 
-      <Section title="Barcode 5" description={getLen(props.settings.code5)}>
+      <Section title="Barcode 5" description={checkCode(props.settings.code5)}>
         <TextInput settingsKey="name5" title="Name"  placeholder="e.g., 7-Eleven" />
         <TextInput settingsKey="code5" title="Barcode" placeholder="e.g., 12345678" />
         <ColorSelect settingsKey="color5" colors={allColors} />
